4a196cd27873a420da1cd5737122b0353b50b6db
Andrew Chmutov <ulman.andre@gmail.com>
2023-12-04 21:20:18 +0100

    Resizable false

--- src/main/java/com/game/GameFrame.java
+++ src/main/java/com/game/GameFrame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame {
     public GameFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
+        this.setResizable(false);
         this.setTitle("title");
         this.setLocationRelativeTo(null);
         this.setVisible(true);

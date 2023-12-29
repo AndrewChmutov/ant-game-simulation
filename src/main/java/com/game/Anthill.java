64b54333d0406968dbc9348aa721b60e442bd280
Andrew Chmutov <ulman.andre@gmail.com>
2023-12-29 07:33:00 +0100

    Side info about anthills

--- src/main/java/com/game/Anthill.java
+++ src/main/java/com/game/Anthill.java
@@ -3,6 +3,7 @@ package com.game;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 
@@ -11,6 +12,8 @@ import com.game.Ant.Team;
 public class Anthill extends Entity {
     Integer collectedLavrae;
     Team team;
+    ArrayList<Ant> ants;
+
     
     BufferedImage icon;
 
@@ -40,11 +43,14 @@ public class Anthill extends Entity {
             icon,
             settings.getTileSize() * 2,
             settings.getTileSize() * 2);
+
+        ants = new ArrayList<>();
     }
 
 
     public void produceAnt() {
         Ant ant = new Ant(game, position, team);
+        ants.add(ant);
         position.insertEntity(ant);
 
         Thread t = new Thread(ant);
@@ -69,17 +75,38 @@ public class Anthill extends Entity {
         panel.fillRect(position.getPoint(), color);
     }
 
-    @Override
-    public void setupInfo(InfoBundler bundler) {
+    public JLabel getDefaultlLabel() {
         JLabel label = new JLabel();
         label.setForeground(Color.white);
         label.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 15));
-        entityInfo.addComponent(new InfoComponent(label) {
+
+        return label;
+    }
+
+    @Override
+    public void setupInfo(InfoBundler bundler) {
+        entityInfo.addComponent(new InfoComponent(getDefaultlLabel()) {
             @Override
             void updateComponent() {
-                ((JLabel)component).setText("Collected lavrae: " + collectedLavrae);
+                synchronized (this) {
+                    ((JLabel)component).setText("Collected lavrae: " + collectedLavrae);
+                }
             }
         });
+
+        entityInfo.addComponent(new InfoComponent(getDefaultlLabel()) {
+            @Override
+            void updateComponent() {
+                synchronized (this) {
+                    ((JLabel)component).setText("Alive ants: " + getAntCount());
+                }
+            }
+        });
+
         bundler.bundle(entityInfo.getComponents());
+    }
+
+    public synchronized int getAntCount() {
+        return ants.size();
     }
 }
